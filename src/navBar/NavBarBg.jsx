0b5de89576b1b01352355ce01d234def85ab2273
Ola Olasunkanmi <finixd531@gmail.com>
2023-11-03 17:18:40 +0100

    fixed link issue for navbar reload on refresh

--- src/navBar/NavBarBg.jsx
+++ src/navBar/NavBarBg.jsx
@@ -1,21 +1,22 @@
  import "../styles/NavBar.css"
+ import { NavLink } from "react-router-dom";
 const NavBarBg = () => {
     // web version navigation file contains only html and css and disappears based on break points 
 
     return ( 
         <div className="d-xl-flex navarBs justify-content-xl-around lign-items-xl-center" >
-            <a href="/" className="strokes">Home</a>
-            <a href="/services" className="strokes">Services</a>
-            <a href="/aboutUs" className="strokes">About us</a>
+            <NavLink to="/" className="strokes">Home</NavLink >
+            <NavLink  to="/services" className="strokes">Services</NavLink >
+            <NavLink  to="/aboutUs" className="strokes">About us</NavLink >
             <div  className="d-xl-flex justify-content-xl-center lign-items-xl-center">
-                <a href="/" >
+                <NavLink  to="/" >
                     <img src="/images/company logo.png" alt="Bas Insurance Logo"/>
-                </a>
+                </NavLink >
                 
             </div> 
-            <a href="/blog" className="strokes">Blog</a>
-            <a href="/contactUs" className="strokes">Contact us</a>
-            <a href="/Partners" className="strokes">Our Partners</a>
+            <NavLink  to="/blog" className="strokes">Blog</NavLink >
+            <NavLink  to="/contactUs" className="strokes">Contact us</NavLink >
+            <NavLink  to="/Partners" className="strokes">Our Partners</NavLink >
         </div>
      );
 }
