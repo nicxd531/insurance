dc425c8c7ed93dae232946518b062b3c7ad8b627
Ola Olasunkanmi <finixd531@gmail.com>
2024-02-10 15:23:49 +0100

    just fixed small nav bar

--- src/navBar/NavBarSs.jsx
+++ src/navBar/NavBarSs.jsx
@@ -1,7 +1,7 @@
 import { IconButton,Box, Divider } from "@mui/material";
 import {Twirl as Hamburger} from "hamburger-react"
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavBarSs = () => {
     // web version navigation file contains only html and css and disappears based on break points 
@@ -13,20 +13,18 @@ const NavBarSs = () => {
             <img src="/images/company logo.png" alt="Bas Insurance Logo"/>
             <Hamburger toggled={isOpen} toggle={setOpen}/>
             <div className={`menu z-1 ${isOpen?"menuOpen" : "menuClose"}`}>
-                <Box sx={{mt:6,flexDirection:"column",width:"100%"}} className="d-flex justify-content-center align-items-center">
-                    <Link href="/" style={{color:"black",textDecoration:"none",fontWeight:"bold",width:"100%",borderRadius:"0"}}> <IconButton sx={{mx:0,width:"100%"}}> Home</IconButton></Link>
+                <Box sx={{mt:0,flexDirection:"column",width:"100%"}} className="d-flex justify-content-center align-items-center">
+                    <NavLink to="/" className="strokes" ><IconButton sx={{mx:0,width:"100%"}}>Home</IconButton></NavLink >
                     <Divider variant="inset" sx={{width:"100%",margin:"auto"}}/>
-                    <Link href="/services" style={{color:"black",textDecoration:"none",fontWeight:"bold"}}><IconButton href="/services" sx={{mx:0}}>Services</IconButton></Link>
+                    <NavLink  to="/services" className="strokes"><IconButton sx={{mx:0}}>Services</IconButton></NavLink >
                     <Divider variant="inset" sx={{width:"100%",margin:"auto"}}/>
-                   <Link href="/aboutUs" style={{color:"black",textDecoration:"none",fontWeight:"bold"}}> <IconButton sx={{mx:0}}>About us</IconButton></Link>
+                    <NavLink  to="/aboutUs" className="strokes"><IconButton sx={{mx:0}}>About us</IconButton></NavLink >
                     <Divider variant="inset" sx={{width:"100%",margin:"auto"}}/>
-                    <Link href="/blog" style={{color:"black",textDecoration:"none",fontWeight:"bold"}}><IconButton sx={{mx:0}}>Blog</IconButton></Link>
+                    <NavLink  to="/blog" className="strokes"><IconButton sx={{mx:0}}>Blog</IconButton></NavLink >
                     <Divider variant="inset" sx={{width:"100%",margin:"auto"}}/>
-                   <Link href="/contactUs" style={{color:"black",textDecoration:"none",fontWeight:"bold"}}> <IconButton sx={{mx:0}}>Contact us</IconButton></Link>
+                    <NavLink  to="/contactUs" className="strokes"><IconButton sx={{mx:0}}>Contact us</IconButton></NavLink >
                     <Divider variant="inset" sx={{width:"100%",margin:"auto"}}/>
-                   <Link href="/Partners" style={{color:"black",textDecoration:"none",fontWeight:"bold"}}> <IconButton sx={{mx:0}}>partners</IconButton></Link>
-                    <Divider variant="inset" sx={{width:"100%",margin:"auto"}}/>
-
+                    <NavLink  to="/Partners" className="strokes"><IconButton sx={{mx:0}}>Our Partners</IconButton></NavLink >
                 </Box>
             </div>
         </div>
